16425aa2b2f8790f169672fdfd2d79f5203f8697
Your Name <you@example.com>
2020-05-10 16:54:08 +0530

    did changes for recuesive copy

--- src/com/filecopy/sample/FileCopy.java
+++ src/com/filecopy/sample/FileCopy.java
@@ -2,17 +2,15 @@ package com.filecopy.sample;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -33,31 +31,12 @@ public class FileCopy {
 		 File folderA = new File(input.next());
 		 System.out.println("Enter the destination folder path : ");
 		 File folderB = new File(input.next());
-		Map<String,File>fileNameAndFileMapA= Stream.of(folderA.listFiles()).collect(Collectors.toMap(File::getName, Function.identity()));
-		Map<String,File>fileNameAndFileMapB=  Stream.of(folderB.listFiles()).collect(Collectors.toMap(File::getName, Function.identity()));
-
-		fileNameAndFileMapA.entrySet().forEach(entryObj->{
-			
-			File existingFleObj=fileNameAndFileMapB.get(entryObj.getKey());
-			if(existingFleObj!=null) {
-				if(entryObj.getValue().lastModified() > existingFleObj.lastModified()) {
-					try {
-						Files.delete(Paths.get(existingFleObj.getAbsolutePath()));
-					existingFleObj.delete();
-					filesCopied=true;
-					copyOrReplaceFileToFolder(folderB,entryObj.getValue());
-					replacedFiles.add(existingFleObj.getName());
-					}catch (Exception e) {
-						failedFileNames.add(existingFleObj.getName());
-						failedFileNames.add("Unable to replace above file because it is being used by another process in destination folder.");
-						
-					}
-				}
-			}else {
-				filesCopied=true;
-				copyOrReplaceFileToFolder(folderB,entryObj.getValue());
-			}
-		});
+		/*
+		 * try { copyDir(Paths.get(folderA.getAbsolutePath()),
+		 * Paths.get(folderB.getAbsolutePath())); } catch (IOException e1) { // TODO
+		 * Auto-generated catch block e1.printStackTrace(); }
+		 */
+		 copyFilesRecursively(folderA, folderB);
 		System.out.println();
 		System.out.println();
 	 if( ! failedFileNames.isEmpty()) {
@@ -78,7 +57,43 @@ public class FileCopy {
 		
 	 }
 	 
-	 private static void copyOrReplaceFileToFolder(File targetFolder, File existingFleObj) {
+	 
+	 private static void  copyFilesRecursively(File source, File destination) {
+			Map<String,File>fileNameAndFileMapA= Stream.of(source.listFiles()).collect(Collectors.toMap(File::getName, Function.identity()));
+			Map<String,File>fileNameAndFileMapB=  Stream.of(destination.listFiles()).collect(Collectors.toMap(File::getName, Function.identity()));
+
+			fileNameAndFileMapA.entrySet().forEach(entryObj->{
+				System.out.println("Keyname "+entryObj.getKey());
+				File existingFleObj=fileNameAndFileMapB.get(entryObj.getKey());
+				
+				if(existingFleObj!=null) {
+					if(existingFleObj.isDirectory()) {
+						copyFilesRecursively(entryObj.getValue(),existingFleObj);
+					}
+					else if(entryObj.getValue().lastModified() > existingFleObj.lastModified()) {
+						try {
+							Files.delete(Paths.get(existingFleObj.getAbsolutePath()));
+						existingFleObj.delete();
+						filesCopied=true;
+						copyOrReplaceFileToFolder(destination,entryObj.getValue());
+						replacedFiles.add(existingFleObj.getName());
+						}catch (Exception e) {
+							failedFileNames.add(existingFleObj.getName());
+							failedFileNames.add("Unable to replace above file because it is being used by another process in destination folder.");
+							
+						}
+					}
+				}else {
+					filesCopied=true;
+					Path createdPath=copyOrReplaceFileToFolder(destination,entryObj.getValue());
+					if(entryObj.getValue().isDirectory() && createdPath!=null) {
+						copyFilesRecursively(entryObj.getValue(),new File(createdPath.toString()));
+					}
+				}
+			});
+	 }
+	 
+	 private static Path copyOrReplaceFileToFolder(File targetFolder, File existingFleObj) {
        System.out.println("Copying file "+existingFleObj.getName());
       String targetFile= targetFolder.getAbsolutePath()+"\\"+existingFleObj.getName();
       String existingFile=existingFleObj.getAbsolutePath();
@@ -86,16 +101,30 @@ public class FileCopy {
         Path source= Paths.get(existingFile);
 		 try {
 			 ConsoleHelper.animate();
-			Files.copy(source, target);
+			Path path= Files.copy(source, target);
 			ConsoleHelper.kill("Copied Successfully...");
+			return path;
 		} catch (IOException e) {
 			failedFileNames.add(existingFleObj.getName());
 			ConsoleHelper.kill("Failed to copy...");
-			//System.out.println("Failed to copy the file please try again"+ existingFile);
 			e.printStackTrace();
+			return null;
 		}
 	}
 
+	 public static void copyDir(Path src, Path dest) throws IOException {
+			Files.walk(src)
+					.forEach(source -> {
+						try {
+							String path=source.toString();
+							System.out.println(path);
+//							Files.copy(source, dest.resolve(src.relativize(source)),
+//											StandardCopyOption.REPLACE_EXISTING);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					});
+		}
 	public static String getModifiedDate(long milliseconds) {
 		 DateFormat format=new SimpleDateFormat("MMMM dd, yyyy hh:mm a");
 			return format.format(milliseconds);
